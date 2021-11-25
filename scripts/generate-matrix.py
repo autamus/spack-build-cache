1f9c54d581c68e2113541f8104b3b3fa4b1eb4c1
vsoch <vsoch@users.noreply.github.com>
2021-11-25 15:45:35 -0700

    first shot at updated branch, but going runing back later
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ scripts/generate-matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import random
+import requests
+import sys
+import json
+
+def main():
+
+    # Get versions of package
+    response = requests.get("https://spack.github.io/packages/data/packages.json")
+    if response.status_code != 200:
+        sys.exit("Failed to get packages")
+    packages = response.json()
+    matrix = random.sample(packages, 25)
+    print(matrix)
+    print("::set-output name=packages::%s\n" % json.dumps(matrix))
+
+if __name__ == "__main__":
+    main()
