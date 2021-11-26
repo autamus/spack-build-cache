3d66d6d9d5591b678768de1526d7d6b9ff67c907
vsoch <vsoch@users.noreply.github.com>
2021-11-25 17:10:54 -0700

    update to have different behavior on PR vs push
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- scripts/generate-matrix.py
+++ scripts/generate-matrix.py
@@ -5,17 +5,19 @@ import requests
 import sys
 import json
 
-def main():
+def main(event_type):
 
-    # Get versions of package
-    response = requests.get("https://spack.github.io/packages/data/packages.json")
-    if response.status_code != 200:
-        sys.exit("Failed to get packages")
-    packages = response.json()
-    matrix = random.sample(packages, 25)
+    if event_type == "PullRequest":
+        matrix = ['zlib']
+    else:
+        response = requests.get("https://spack.github.io/packages/data/packages.json")
+        if response.status_code != 200:
+            sys.exit("Failed to get packages")
+        packages = response.json()
+        matrix = random.sample(packages, 25)
+    
     print(matrix)
-    matrix = ['zlib']
     print("::set-output name=packages::%s\n" % json.dumps(matrix))
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
